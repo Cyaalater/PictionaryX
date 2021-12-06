fdf8de9912fe5ff2301049534630681b548ed82a
mineitan <mineitan@gmail.com>
2021-12-06 19:05:12 +0200

    Adding lobby system

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
+'use strict';
 const express = require('express')
 const app = express()
 
 const Socket = require('socket.io')
 
-const uuid = require('uuid').v3
+const uuid = require('uuid').v4
 // Server info setup
 let info = {
-    users:0,
+    users: 0,
 
 }
+// Objects used as dictionary
 let names = {}
 let rooms = {}
+var socketsRooms = {}
+var socketData = {} // Struct: = {1:{room:'',name:''},2:{room:'',name:''}}
 // Player info setup
-class Player{
-    constructor(sid,name) {
-        this.sid = sid
-        this.name = name
-    }
-    get all(){ return({sid:this.sid,name:this.name})}
-}
+
+
+/**
+ * Will connect a socket to a specified room
+ * @param socket A connected socket.io socket
+ * @param room An object that represents a room from the `rooms` instance variable object
+ */
+const joinRoom = async (socket, room) => {
+    room.sockets.push(socket);
+    await socket.join(room.id)
+    // store the room id in the socket for future use
+    socketData[socket.id].room = room.id;
+    console.log(socket.id, "Joined", room.id);
+};
 
 
 // Express bullshittery
@@ -43,20 +54,48 @@ let server = app.listen(3000, () => {
 
 // Socketio bullshittery
 var io = Socket(server)
-io.on('connection',(socket) => {
+io.on('connection', (socket) => {
+
     console.log(`Hello, ${socket.id}}`)
-    io.sockets.emit('con',socket.id)
+
+    socket.on('newPlayer', function (data) {
+        console.log(data)
+        socketData[socket.id] = {
+            room:'',
+            name:data.name
+        }
+    })
+
+    socket.on('createRoom', (roomName,callback) => {
+        if(socketData[socket.id] == undefined){
+            return;
+        }
+        const newRoom = {
+            name: roomName,
+            id: uuid(),
+            sockets: []
+        }
+        rooms[newRoom.id] = newRoom
+        joinRoom(socket, newRoom)
+        console.log(rooms)
+        callback(`LOBBY ID: ${newRoom.id}\nNAME: ${roomName}`)
+    })
+
+    socket.on('joinRoom', (roomId,callback) => {
+        if(socketData[socket.id] == undefined){
+            return;
+        }
+        const room = rooms[roomId];
+        joinRoom(socket, room);
+        console.log(rooms[roomId])
+        callback(`LOBBY ID: ${roomId}\nNAME: ${room.name}`)
+    });
+
+    socket.on('sendText', (textValue) => {
+        console.log(`Group id:\n${JSON.stringify(socketData[socket.id].room)}\nText: ${textValue}`)
+        // const room = rooms[socket.roomId]
+        // const roomSocketId = room.id
+        io.to(socketData[socket.id].room).emit("textSend",{name:JSON.stringify(socketData[socket.id].name),text:textValue})
+    })
 })
 
-io.on('newPlayer',(socket) => {
-    console.log(socket.id)
-})
-
-io.on('newRoom',(roomName,callback) => {
-    const newRoom = {
-        name: roomName,
-        id: uuid(),
-        sockets: []
-    }
-    rooms[newRoom.id] = newRoom
-})
--- public/home.js
+++ public/home.js
@@ -5,12 +5,37 @@ var socket = io.connect('http://localhost:3000');
 var nameInput  = document.querySelector("#name");
 var nameButton = document.querySelector("#continue_name");
 
+var joinInput = document.querySelector("#join");
+var joinButton = document.querySelector("#join_room")
+
+var textInput = document.querySelector("#text");
+var textButton = document.querySelector("#send");
+
+var messages = document.querySelector("#messages")
+
 nameButton.addEventListener('click',() => {
-    socket.emit('newPlayer',`${nameInput.innerText}`)
+    socket.emit('newPlayer',{name:nameInput.value})
+    console.log(nameInput.value)
+})
+
+joinButton.addEventListener('click',(evt) => {
+    socket.emit('joinRoom',joinInput.value,(dataText) => {
+        console.log(dataText)
+    })
+})
+
+textButton.addEventListener('click',(evt) => {
+    socket.emit('sendText',textInput.value)
 })
 
 document.getElementById('continue_room').addEventListener('click',(event) => {
-    socket.emit('newRoom',`${document.getElementById('room').innerText}`)
+    socket.emit('createRoom',`${document.getElementById('room').value}`,(dataText) => {
+        console.log(dataText)
+    })
+})
+
+socket.on('textSend',(data) => {
+    messages.innerHTML += `<p>${data.name} || ${data.text}</p>`
 })
 
 // socket.on('con',(data) => {
